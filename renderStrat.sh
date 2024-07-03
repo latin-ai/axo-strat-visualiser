8deb12dc0ff11542a83f48ee90e5be9dbfe7c78f
Jaime Andres Posada Castro <support@cardanolatino.com>
2024-07-03 10:20:31 -0500

    Added absolute paths-send email

--- renderStrat.sh
+++ renderStrat.sh
@@ -4,28 +4,36 @@ echo "start renderStrat.sh script ..."
 
 TICK=${3:-0.01}
 SCALE_FACTOR=${4:-1}
-ASSET=${2:-"-"}
+ASSET=$2
 STRAT=$1
 echo "STRAT: ${STRAT}"
 echo "ASSET: ${ASSET}"
 echo "TICK:  ${TICK}"
 echo "SCALE_FACTOR:  ${SCALE_FACTOR}"
 
-. ./fetch_data.sh
-fetch_data $STRAT $ASSET "./renderStrat_database/"
+# Ejecutar el script fetch_data.sh para recopilar los datos
+. /home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/fetch_data.sh
+fetch_data $STRAT $ASSET "/home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/renderStrat_database/"
 
 echo ""
 echo "::::"
 echo "curl requests ended. fetch_data() exited. back into renderStrat.sh"
 
-node renderStrat1TimeStorageFactory.js $STRAT $ASSET $TICK $SCALE_FACTOR
+# Ejecutar el script renderStrat1TimeStorageFactory.js para preparar los datos para el renderizado
+node /home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/renderStrat1TimeStorageFactory.js $STRAT $ASSET $TICK $SCALE_FACTOR
 
-#brave ./renderStrat_page/index.html
+
+# # Enviar correo con los resultados
+# SUBJECT="Axo Strategy"
+# BODY="Strategy ID: $STRAT, Asset: $ASSET, Profit Percentage: $PROFIT_PERCENT%"
+# echo -e "Subject:${SUBJECT}\n\n${BODY}" | ssmtp support@cardanolatino.com
+
+flatpak run com.google.Chrome /home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/renderStrat_page/index.html
 echo ""
 echo "link to html page:"
-echo "$(pwd)/renderStrat_page/index.html"
+echo "/home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/renderStrat_page/index.html"
 
-# remove fetch files
+# Opcional: Eliminar archivos de datos fetch para limpiar
 # echo "deleting fetch_data jsons ..."
-# rm ./renderStrat_database/*.json
+# rm /home/jaime/Documentos/Cardano-Latino/axo-strat-visualiser/renderStrat_database/*.json
 # echo "deleted."
